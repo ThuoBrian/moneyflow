bc2e0946cc04735d5a8ded84d793b137badedf9b
Brian Thuo <thuogachau@gmail.com>
2025-04-15 07:54:52 +0300

    Refactor user profile endpoints by removing unused functions and consolidating update logic

--- src/controllers/me.rs
+++ src/controllers/me.rs
@@ -5,32 +5,7 @@ pub async fn get_profile() -> impl Responder {
     "Profile"
 }
 
-#[get("/me")]
-pub async fn get_user_requests() -> impl Responder {
-    "All requests"
-}
-
-#[get("/me")]
-pub async fn get_user_activation_status() -> impl Responder {
-    "is user activate"
-}
-
 #[post("/me")]
-pub async fn update_profile() -> impl Responder {
-    "Update Profile"
-}
-
-#[post("/me")]
-pub async fn query_user_transactions() -> impl Responder {
-    " Transactions: "
-}
-
-#[post("/me")]
-pub async fn query_user_pending_items() -> impl Responder {
-    "Categories"
-}
-
-#[post("/me")]
-pub async fn query_user_available_balance() -> impl Responder {
-    "Available Balances"
+pub async fn update_user_profile() -> impl Responder {
+    "Update User Profile"
 }
