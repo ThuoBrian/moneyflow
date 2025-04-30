bd2b789f27e0e3325a556279f8e793077d81f28f
Brian Thuo <thuogachau@gmail.com>
2025-04-30 18:35:58 +0300

    Refactor password validation in create_user function for clarity and efficiency

--- src/db/user.rs
+++ src/db/user.rs
@@ -20,68 +20,50 @@ pub async fn create_user(
     if !email_regex.is_match(&userinfo.email) {
         return Err(sqlx::Error::Protocol("Invalid email format".into()));
     }
-    if userinfo.password.len() < 8 {
+    if *&userinfo.password.len() < 8 {
         return Err(sqlx::Error::Protocol(
             "Password must be at least 8 characters long".into(),
         ));
     }
-    // const MIN_PASSWORD_LENGTH: usize = 10;
-    // const SPECIAL_CHARS: &str = "!@#$%^&*()_+-=[]{}|;':\",.<>?/`~";
+    let password = &userinfo.password;
 
-    // let password = &userinfo.password;
+    // Single pass to check all character requirements
+    let mut has_uppercase = false;
+    let mut has_lowercase = false;
+    let mut has_digit = false;
 
-    // if password.len() < MIN_PASSWORD_LENGTH {
-    //     return Err(sqlx::Error::Protocol(
-    //         "Password Must be 10 char and over".into(),
-    //     ));
-    // }
+    for c in password.chars() {
+        if c.is_whitespace() {
+            return Err(sqlx::Error::Protocol(
+                "Password must not contain any whitespace characters".into(),
+            ));
+        }
+        if c.is_uppercase() {
+            has_uppercase = true;
+        } else if c.is_lowercase() {
+            has_lowercase = true;
+        }
+        if c.is_digit(10) {
+            has_digit = true;
+        }
+    }
 
-    // // Single pass to check all character requirements
-    // let mut has_uppercase = false;
-    // let mut has_lowercase = false;
-    // let mut has_digit = false;
-    // let mut has_special = false;
-
-    // for c in password.chars() {
-    //     if c.is_whitespace() {
-    //         return Err(sqlx::Error::Protocol(
-    //             "Password must not contain any whitespace characters".into(),
-    //         ));
-    //     }
-    //     if c.is_uppercase() {
-    //         has_uppercase = true;
-    //     } else if c.is_lowercase() {
-    //         has_lowercase = true;
-    //     }
-    //     if c.is_digit(10) {
-    //         has_digit = true;
-    //     }
-    //     if SPECIAL_CHARS.contains(c) {
-    //         has_special = true;
-    //     }
-    // }
-
-    // // Check all requirements in one go
-    // if !has_uppercase {
-    //     return Err(sqlx::Error::Protocol(
-    //         "Password must contain at least one uppercase letter".to_string(),
-    //     ));
-    // }
-    // if !has_lowercase {
-    //     return Err(sqlx::Error::Protocol(
-    //         "Password must contain at least one lowercase letter".to_string(),
-    //     ));
-    // }
-    // if !has_digit {
-    //     return Err(sqlx::Error::Protocol(
-    //         "Password must contain at least one digit".to_string(),
-    //     ));
-    // }
-    // if !has_special {
-    //     return Err(sqlx::Error::Protocol(
-    //         "Password must contain at least one special character".to_string(),
-    //     ));
-    // }
+    // Check all requirements in one go
+    if !has_uppercase {
+        return Err(sqlx::Error::Protocol(
+            "Password must contain at least one uppercase letter".to_string(),
+        ));
+    }
+    if !has_lowercase {
+        return Err(sqlx::Error::Protocol(
+            "Password must contain at least one lowercase letter".to_string(),
+        ));
+    }
+    if !has_digit {
+        return Err(sqlx::Error::Protocol(
+            "Password must contain at least one digit".to_string(),
+        ));
+    }
 
     // Encrypt the password only after all validations pass
     let encrypted_password = bcrypt::hash(&userinfo.password, bcrypt::DEFAULT_COST)
